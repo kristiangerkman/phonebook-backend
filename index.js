4baf74abaee8904d45595d7d01beb669c66793ec
Kristedd <kristiangerkman1@gmail.com>
2019-11-04 00:57:07 +0200

    commit

--- index.js
+++ index.js
@@ -162,7 +162,7 @@ app.put("/api/persons/:id", (req, res, next) => {
 app.use(unknownEndpoint);
 app.use(errorHandler);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
